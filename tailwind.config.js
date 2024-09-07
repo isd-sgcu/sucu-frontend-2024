06d51e56176bd3cf95b54178edd4d54124dc7af3
Gear <84141000+macgeargear@users.noreply.github.com>
2024-09-08 01:43:01 +0700

    feat: borderRadius styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 import { colorScheme } from './src/styles/tailwind/color';
 import { boxShadow } from './src/styles/tailwind/shadow';
+import { borderRadius } from './src/styles/tailwind/borderRadius';
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {
 			colors: colorScheme,
-			boxShadow: boxShadow
+			boxShadow: boxShadow,
+			borderRadius: borderRadius
 		}
 	},
 	plugins: []
