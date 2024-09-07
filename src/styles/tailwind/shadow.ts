64cd649cc9fa62b65cbe4efa4deffb5ebfdfb351
Sirawit Chanaburanasak <ext.sirawit@skooldio.com>
2024-09-07 22:37:24 +0700

    feat: shadow

---
+++ src/styles/tailwind/shadow.ts
@@ -0,0 +1,6 @@
+export const boxShadow = {
+    'button-shadow': '0px 4px 12px 0px rgba(0, 0, 0, 0.15)',
+    'card-shadow': '0px 8px 24px 0px rgba(0, 0, 0, 0.15) ',  
+    // we cannot specify the hover: state directly on box shadow
+    'card-shadow-hover': '0px 8px 24px 0px rgba(0, 0, 0, 0.25)',
+}
