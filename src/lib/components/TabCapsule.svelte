6e0a79c2c893c94a73f8ce9d2936d2e0fc5ff96e
Gear <84141000+macgeargear@users.noreply.github.com>
2024-09-12 16:26:53 +0700

    fix: remove hover effect

--- src/lib/components/TabCapsule.svelte
+++ src/lib/components/TabCapsule.svelte
@@ -5,26 +5,22 @@
 
 	export let active: boolean = false;
 	export let label: string;
-
-	let isHovered = false;
 </script>
 
 <button
 	class={cn(
-		'gap-2 flex items-center rounded-full w-fit px-2 border border-sucu-gray-light text-sucu-gray-light cursor-pointer hover:border-sucu-pink-01 hover:text-sucu-pink-01',
+		'gap-2 flex items-center rounded-full w-fit px-2 border border-sucu-gray-light text-sucu-gray-light cursor-pointer',
 		{
 			'border-sucu-pink-01 text-sucu-pink-01': active
 		}
 	)}
-	on:mouseenter={() => (isHovered = true)}
-	on:mouseleave={() => (isHovered = false)}
 	on:click
 >
 	<span class="text-sm md:text-base">{label}</span>
 	<Fa
 		icon={faPlus}
 		class={cn('transition-transform transform', {
-			'rotate-45': isHovered || active
+			'rotate-45': active
 		})}
 	/>
 </button>
