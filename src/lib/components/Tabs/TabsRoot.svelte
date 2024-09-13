859366b23d351e8c15f205da54e4e83fa262182d
Gear <84141000+macgeargear@users.noreply.github.com>
2024-09-13 14:12:25 +0700

    feat(tab): tab root

---
+++ src/lib/components/Tabs/TabsRoot.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { setTabsContext } from './tabsContext';
+
+	export let defaultActiveTab: string = '';
+
+	const tabs = setTabsContext();
+
+	if (defaultActiveTab) {
+		tabs.activeTab.set(defaultActiveTab);
+	}
+</script>
+
+<div>
+	<slot />
+</div>
