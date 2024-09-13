2015db1f02a6562d63fc189c10ee05b5cbbf6197
Gear <84141000+macgeargear@users.noreply.github.com>
2024-09-13 14:12:43 +0700

    feat(tab): tab context

---
+++ src/lib/components/Tabs/tabsContext.ts
@@ -0,0 +1,44 @@
+import { writable, get, type Writable } from 'svelte/store';
+import { setContext, getContext } from 'svelte';
+
+interface Tab {
+	value: string;
+}
+
+export interface TabsContext {
+	activeTab: Writable<string>;
+	tabs: Writable<Tab[]>;
+	registerTab: (tab: Tab) => void;
+	selectTab: (value: string) => void;
+}
+
+const createTabs = (): TabsContext => {
+	const activeTab = writable<string>('');
+	const tabs = writable<Tab[]>([]);
+
+	const registerTab = (tab: Tab) => {
+		tabs.update((currTabs) => [...currTabs, tab]);
+		if (get(activeTab) === null) activeTab.set(tab.value);
+	};
+
+	const selectTab = (value: string) => {
+		activeTab.set(value);
+	};
+
+	return {
+		activeTab,
+		tabs,
+		registerTab,
+		selectTab
+	};
+};
+
+export const setTabsContext = (): TabsContext => {
+	const tabs = createTabs();
+	setContext<TabsContext>('tabs', tabs);
+	return tabs;
+};
+
+export const getTabsContext = (): TabsContext => {
+	return getContext<TabsContext>('tabs');
+};
