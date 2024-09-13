1691541b2f64804724e859be3cfe1e1a6f639d11
Gear <84141000+macgeargear@users.noreply.github.com>
2024-09-13 14:11:47 +0700

    feat(tab): tab content

---
+++ src/lib/components/Tabs/TabsContent.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { getTabsContext, type TabsContext } from './tabsContext';
+
+	export let value: string;
+	const { activeTab } = getTabsContext();
+
+	$: isActive = $activeTab === value;
+</script>
+
+{#if isActive}
+	<div class="p-4">
+		<slot />
+	</div>
+{/if}
