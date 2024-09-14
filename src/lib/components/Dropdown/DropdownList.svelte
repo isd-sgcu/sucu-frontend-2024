1383d8bbd4fed4ec180352ee26780005dd04e40a
Thanaphom Hirunyathon <thanaphom.h@gmail.com>
2024-09-14 20:11:36 +0700

    feat: dropdownList

---
+++ src/lib/components/Dropdown/DropdownList.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import DropdownItem from "$lib/components/Dropdown/DropdownItem.svelte";
+
+    export let items : string[] = ["Hi" , "jpoas" , "adfsd" , "asdafsds"]
+    export let currentChoice : string|null = null
+
+</script>
+
+
+<div class="flex flex-col outline outline-1 outline-sucu-gray rounded-sm gap-1 overflow-hidden bg-white">
+    {#each items as item}
+        <DropdownItem text={item} bind:currentSelectedChoice={currentChoice}/>
+    {/each}
+</div>
