a819fc88a034d4058867c45d23446bda2c41c68e
Gear <84141000+macgeargear@users.noreply.github.com>
2024-09-16 14:39:03 +0700

    feat: max width wrapper

---
+++ src/lib/components/MaxWidthWrapper.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+
+	export let className: string = '';
+	export { className as class };
+</script>
+
+<div
+	class={cn(
+		'h-full mx-auto w-full max-w-screen-xl px-6 lg:px-[200px] mt-6 lg:mt-[72px]',
+		className
+	)}
+>
+	<slot />
+</div>
