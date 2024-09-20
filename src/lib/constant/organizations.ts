9ee649d49c2852af8630d79149abd5aa6f16f848
Amphi <6633287021@student.chula.ac.th>
2024-09-20 21:41:54 +0700

    fix: fix lint

--- src/lib/constant/organizations.ts
+++ src/lib/constant/organizations.ts
@@ -3,20 +3,20 @@ import smo from '../assets/images/smo.png';
 import sgcu from '../assets/images/sgcu.png';
 
 export const organizations = [
-    {
-        imageURL: smo,
-        orgName: 'สโมสรนิสิต \n จุฬาลงกรณ์มหาวิทยาลัย',
-        linkHref: 'https://www.google.com'
-    },
-    {
-        imageURL: sgcu,
-        orgName: 'องค์การบริหารสโมสรนิสิต จุฬาลงกรณ์มหาวิทยาลัย (อบจ.)',
-        linkHref: 'https://www.chula.ac.th/academics/academic-units/sgcu/'
-    },
-    {
-        imageURL: sapanisit,
-        orgName: 'สภานิสิต \n จุฬาลงกรณ์มหาวิทยาลัย',
-        linkHref:
-            'https://www.sa.chula.ac.th/%E0%B8%AA%E0%B8%A0%E0%B8%B2%E0%B8%99%E0%B8%B4%E0%B8%AA%E0%B8%B4%E0%B8%95/'
-    }
+	{
+		imageURL: smo,
+		orgName: 'สโมสรนิสิต \n จุฬาลงกรณ์มหาวิทยาลัย',
+		linkHref: 'https://www.google.com'
+	},
+	{
+		imageURL: sgcu,
+		orgName: 'องค์การบริหารสโมสรนิสิต จุฬาลงกรณ์มหาวิทยาลัย (อบจ.)',
+		linkHref: 'https://www.chula.ac.th/academics/academic-units/sgcu/'
+	},
+	{
+		imageURL: sapanisit,
+		orgName: 'สภานิสิต \n จุฬาลงกรณ์มหาวิทยาลัย',
+		linkHref:
+			'https://www.sa.chula.ac.th/%E0%B8%AA%E0%B8%A0%E0%B8%B2%E0%B8%99%E0%B8%B4%E0%B8%AA%E0%B8%B4%E0%B8%95/'
+	}
 ];
