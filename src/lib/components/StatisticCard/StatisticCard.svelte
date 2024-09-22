6abd83119c8ab47b6a4e1b5b2e2f053bcc3b761d
KinkPapawin <kinkcr7@gmail.com>
2024-09-22 18:16:35 +0700

    feat/statistic card desktop done, for mobile wait for recomponent

--- src/lib/components/StatisticCard/StatisticCard.svelte
+++ src/lib/components/StatisticCard/StatisticCard.svelte
@@ -1,56 +1,46 @@
 <script lang="ts">
-  import { faCalendar, faChalkboard, faPencil } from '@fortawesome/free-solid-svg-icons';
-  import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
+	import { faCalendar, faChalkboard, faPencil } from '@fortawesome/free-solid-svg-icons';
+	import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
 	import thumbnail from '../../assets/images/thumbnail.png';
 
-  export let imageUrl = thumbnail;
-  export let title = '';
-  export let createAt = '';
-  export let hrefUrl = '';
-  export let infoType = '';
-  export let organization = '';
+	export let imageUrl = thumbnail;
+	export let title = '';
+	export let createAt = '';
+	export let hrefUrl = '';
+	export let infoType = '';
+	export let organization = '';
 </script>
 
 <div
-  class="flex flex-row bg-white rounded-md overflow-hidden shadow-card-shadow w-[1040px] h-[200px] p-2 my-4"
+	class="flex flex-col md:flex-row bg-white rounded-md overflow-hidden shadow-card-shadow w-full md:w-[1040px] h-auto md:h-[200px] p-2 my-4"
 >
-  <!-- Image Section -->
-  <div class="w-[276px] h-[180px]">
-    <img src={imageUrl} alt={title} class="w-full h-full object-cover" />
-  </div>
+	<div class="w-full md:w-[276px] h-[180px] mb-4 md:mb-0">
+		<img src={imageUrl} alt={title} class="w-full h-full object-cover" />
+	</div>
+	<div class="px-2 md:px-7 py-2 flex flex-col justify-between w-full">
+		<div>
+			<p class="text-xl md:text-2xl font-semibold text-sucu-gray-dark mb-2">
+				สถิติการเข้าประชุมของ ส.ส. ปีการศึกษา 2566
+			</p>
+			<div class="flex items-center text-lg md:text-xl text-sucu-gray-dark mb-3 md:mb-5">
+				<FontAwesomeIcon icon={faCalendar} class="mr-3" />
+				<p>04 กรกฎาคม 2567</p>
+			</div>
+		</div>
 
-  <!-- Content Section -->
-  <div class="px-7 py-2 flex flex-col justify-between w-full">
-    <!-- Title and Date -->
-    <div>
-      <p class="text-2xl font-semibold text-sucu-gray-dark mb-2">
-        สถิติการเข้าประชุมของ ส.ส. ปีการศึกษา 2566
-      </p>
-
-      <div class="flex items-center text-xl text-sucu-gray-dark mb-5">
-        <FontAwesomeIcon icon={faCalendar} class="mr-3" />
-        <p>04 กรกฎาคม 2567</p>
-      </div>
-    </div>
-
-    <!-- Additional Info -->
-    <div class="flex justify-between">
-      <div>
-        <div class="flex items-center mb-2">
-          <FontAwesomeIcon icon={faChalkboard} class="mr-3" />
-          <p class="text-sucu-gray-dark">สถิติ</p>
-        </div>
-
-        <div class="flex items-center">
-          <FontAwesomeIcon icon={faPencil} class="mr-3" />
-          <p class="text-sucu-gray-dark">สภานิสิตฯ</p>
-        </div>
-      </div>
-
-      <!-- Read More -->
-      <a href={hrefUrl} class="text-black text-xl content-center">
-        อ่านต่อ »
-      </a>
-    </div>
-  </div>
+		<div class="flex flex-row justify-between pt-3 md:py-0">
+			<div class="mb-4 md:mb-0">
+				<div class="flex items-center mb-2">
+					<FontAwesomeIcon icon={faChalkboard} class="mr-3" />
+					<p class="text-sucu-gray-dark">สถิติ</p>
+				</div>
+				<div class="flex items-center">
+					<FontAwesomeIcon icon={faPencil} class="mr-3" />
+					<p class="text-sucu-gray-dark">สภานิสิตฯ</p>
+				</div>
+			</div>
+			<a href={hrefUrl} class="text-black text-lg md:text-xl self-center"> อ่านต่อ » </a>
+		</div>
+    
+	</div>
 </div>
