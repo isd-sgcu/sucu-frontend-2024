04ebe8c2ead03d9c272c91286eaa7b9fa0fc2c56
Chulinuwu <ananzxzoxd@gmail.com>
2024-10-03 19:28:34 +0700

    feat:footer + tab + header

--- src/routes/annoucement/+page.svelte
+++ src/routes/annoucement/+page.svelte
@@ -13,14 +13,22 @@
 	import TabsContent from '$lib/components/Tabs/TabsContent.svelte';
 	import MaxWidthWrapper from '$lib/components/MaxWidthWrapper.svelte';
 	import Navbar from '../../lib/components/Navbar.svelte';
+    import Footer from '../../lib/components/Footer/Footer.svelte';
 	import pic1 from './../../lib/assets/images/picslide1.jpg';
 	import pic2 from './../../lib/assets/images/picslide2.jpg';
 	import pic3 from './../../lib/assets/images/picslide3.jpg';
 	import pic4 from './../../lib/assets/images/picslide4.jpg';
+	import SearchBar from '$lib/components/SearchBar.svelte';
+	import Dropdown from '$lib/components/Dropdown/Dropdown.svelte';
+
+
 
 	const image = [pic1, pic2, pic3, pic4];
-
+	let searchValue = '';
+    let dropdownValue = 'All';	
 	let currentIndex = 0;
+
+
 
 	function handleLeftClick() {
 		currentIndex = (currentIndex-1) % image.length;
@@ -56,8 +64,8 @@
 	<Navbar />
 
 <div class="relative flex w-full h-[500px]">
-    <button class= "absolute left-5 top-1/2  transform -translate-y-1/2 z-10 cursor-pointer hover:scale-110 transition-all" on:click={handleLeftClick}>
-        <Fa icon={faChevronLeft}  />
+    <button class= "absolute left-5 top-1/2  transform -translate-y-1/2 z-10 cursor-pointe transition-all" on:click={handleLeftClick}>
+        <Fa icon={faChevronLeft} class="text-white scale-150"  />
         </button>
     <div class="relative w-full h-[500px] flex overflow-hidden image-container snap-x">
         
@@ -68,11 +76,40 @@
         {/each}
      
       </div>
-      <button class= "absolute right-5 top-1/2  transform -translate-y-1/2 z-10 cursor-pointer hover:scale-110 transition-all" on:click={handleRightClick}>
-      <Fa icon={faChevronRight}  />
+      <button class= "absolute right-5 top-1/2  transform -translate-y-1/2 z-10 cursor-pointer  transition-all" on:click={handleRightClick}>
+      <Fa icon={faChevronRight} class="text-white scale-150" />
       </button>
     </div>
-	<MaxWidthWrapper class="mt-10 space-y-12">
-		<div>asdasd</div>
+
+
+	<MaxWidthWrapper class="my-10 space-y-12">
+        <div class="flex flex-col gap-3 items-start">
+		<div class="flex items-center gap-4">
+			<button on:click={() => history.back()} class="lg:relative -left-96 scale-150 ">
+				<Fa icon={faCircleArrowLeft} size="lg" />
+			</button>
+			<h1 class={cn(typography({ variant: 'heading3' }), 'md:text-5xl lg:order-first')}>เอกสาร</h1>
+		</div>
+        <p class="text-sucu-gray lg:w-full">
+			เอกสารทั้งหมดในนามสโมสรนิสิตจุฬาฯ อบจ. และสภานิสิตจุฬาฯ ซึ่งเปิดเผยให้นิสิตได้อ่านโดยทั่วกัน
+		</p>
+        </div>
+
+        <SearchBar bind:value={searchValue} />
+
+        <TabsRoot defaultActiveTab="all">
+            <TabsList>
+                <TabsTrigger value="all">ทั้งหมด</TabsTrigger>
+                <TabsTrigger value="sgcu">อบจ.</TabsTrigger>
+                <TabsTrigger value="sccu">สภานิสิต</TabsTrigger>
+            </TabsList>
+        </TabsRoot>
+        <Dropdown
+            items={['All', '2021', '2020', '2019']}
+            bind:currentChoice={dropdownValue}
+            outerClass="w-64 my-12"/>
+
 	</MaxWidthWrapper>
+
+    <Footer />
 </div>
