60dd0a23de1aaeafe7de6e0000219009b38fb974
Chulinuwu <ananzxzoxd@gmail.com>
2024-10-03 20:31:24 +0700

    fix:remove announcement endpoint

--- src/routes/announcement/+page.ts
+++
@@ -1,9 +0,0 @@
-import type { PageLoad } from './$types';
-import 'dayjs/locale/th';
-import { announcements } from '$lib/mock/annoucement';
-
-export const load: PageLoad = async () => {
-	return {
-		announcements
-	};
-};
